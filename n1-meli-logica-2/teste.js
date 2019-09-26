7edcd260e625348b0c0ca1a0913b7703bc73a26d
iprih <priscila.silva076@gmail.com>
2019-09-26 20:08:07 -0300

    novos exercicios

--- n1-meli-logica-2/teste.js
+++ n1-meli-logica-2/teste.js
@@ -1,5 +1,38 @@
-let nome = "Ola Priscila"
+/*let nome = "Ola Priscila"
 
 nome.indexIndexOf("i");
 console.log(nome);
+*/
 
+function imprimeIdade(){
+    var idade = 30;
+    console.log(`Idade ${idade}`)
+}
+
+imprimeIdade();
+
+
+/*2) Crie um programa gerador de estrelas. 
+O usuário informa um número, e o programa gera uma pirâmide de estrelas no seguinte formato:
+// Ex.: Usuário informa 5 e o programa retorna
+// *
+// * *
+// * * *
+// * * * * 
+// * * * * * 
+- Você pode concatenar uma string usando: + ou +=
+*/
+var r = require("readline-sync");
+
+function estrelas(){
+    var qtd = parseInt(r.question("Digite um valor de estrelas: "));
+    var i = 0;
+
+    do{
+        qtd += "*" +i;
+        i++;
+    }
+    while(i < 5);   
+
+}
+estrelas()
