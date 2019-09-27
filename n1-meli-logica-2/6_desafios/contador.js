0a9bf2681bb7c8eee868fe4dcfcb644ec32875f1
iprih <priscila.silva076@gmail.com>
2019-09-27 19:49:27 -0300

    contador e arrays

---
+++ n1-meli-logica-2/6_desafios/contador.js
@@ -0,0 +1,5 @@
+//5 - Crie um programa que conte de 10 até 1 de 1 em 1.
+
+for (let i = 10; i >= 1; i--){
+    console.log(i)
+}
--- n1-meli-logica-2/6_desafios/exemploloops.js
+++ n1-meli-logica-2/6_desafios/exemploloops.js
@@ -5,7 +5,7 @@ while (contador < 6){
 }
 
 //for
-
+//da pra fazer for com 
 for(let contador = 0; contador < 6; contador++){
     console.log("Ola");
 }
