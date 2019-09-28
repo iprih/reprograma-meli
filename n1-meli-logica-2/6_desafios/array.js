3c297c495e4d91fdc18b9fcf9d6b9b471eb1a997
iprih <priscila.silva076@gmail.com>
2019-09-27 21:22:47 -0300

    att

--- n1-meli-logica-2/6_desafios/array.js
+++ n1-meli-logica-2/6_desafios/array.js
@@ -1,5 +1,30 @@
+//let max = Math.max(array.length);
+//pesquisar sobre infitiny
 /*6 - Crie um programa que receba o array = [5, 37, 18, 59, 12, -5] e faça as seguintes operações:
     - Printe o maior;
     - Printe o menor.*/
 
-    
+const lista = [5, 37, 18, 58, 12, -5];
+
+
+
+for (let max = 0; max < lista.length; max++){
+    let min = lista.[max];
+    console.log(`o array ${lista}`);
+    console.log(`a lista ${max}`);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(max);
+
---
+++ n1-meli-logica-2/6_desafios/estrelas_new.js
@@ -0,0 +1,18 @@
+
+/*
+2 - Utilizando uma estrutura de repetição diferente da que utilizou ontem, crie um programa gerador de estrelas.
+O usuário informa um número, e o programa gera uma pirâmide de estrelas no seguinte formato:
+// Ex.: Usuário informa 5 e o programa retorna
+// *
+// * *
+// * * *
+// * * * * 
+// * * * * * 
+- Você pode concatenar uma string usando: + ou +=*/
+
+const r = require("readline-sync");
+
+let estrela = parseInt(r.question("Digite a quantidade de estrelas"));
+let stars = "";
+for(let i = 0; i < estrela; i++ ){
+    }
