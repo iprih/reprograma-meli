88c9fe1d0801aea97852becfa8e69cbebd550552
iprih <priscila.silva076@gmail.com>
2019-09-30 21:28:01 -0300

    resolução 7

---
+++ n1-meli-logica-3/ex07.js
@@ -0,0 +1,11 @@
+
+// 7- Crie um Array de 10 valores, apenas com Números Ímpares.
+// depois imprima os valores na tela
+const arra = [];
+
+for (let i=1; i<21; i++){
+    if (i %2===1){
+        arra.push(i);
+    }
+}
+console.log(arra) 
---
+++ n1-meli-logica-3/ex09.js
@@ -0,0 +1,4 @@
+// 9- dada a array const lista = [56, 89, 34, 12, 23, 55, 9]
+// ordene a lista .
+
+const lista = [56, 89, 34, 12, 23, 55, 9];
